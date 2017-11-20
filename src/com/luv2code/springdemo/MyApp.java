be3de06da02102418d492886f302a294b33fe699
bartoszc <bartosz.cichowicz@kpit.com>
2017-11-20 16:58:14 +0100

    Prototype without spring

--- src/com/luv2code/springdemo/MyApp.java
+++ src/com/luv2code/springdemo/MyApp.java
@@ -3,8 +3,9 @@ package com.luv2code.springdemo;
 public class MyApp {
 
 	public static void main(String[] args) {
-		
-		Coach theCoach = new BaseballCoach();
+		//create object
+		Coach theCoach = new TrackCoach(); //wskazuje na konkretna klase, nie jest konfigurowalne
+		//use the object
 		System.out.println(theCoach.getDailyWorkout());
 		
 
